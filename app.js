6ea2cd5884b87eb0ff6dc9d2c14698df4cb46ac9
SRIRAM-POTHUMANI <84328532+SRIRAM-POTHUMANI@users.noreply.github.com>
2021-11-22 14:23:45 +0530

    cors

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongo=require('./connection');
-import cors from "cors";
+var cors = require('cors');
 
 
 var indexRouter = require('./routes/index');
